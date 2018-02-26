3dd8047d9dc2fbf63d7ff31b370de00ab7ac83f8
Matteo Ceccarello <ceccarel@dei.unipd.it>
2018-02-26 22:19:50 +0100

    Implement streaming and mapreduce drivers

--- experiments/src/main/scala/Algorithm.scala
+++ experiments/src/main/scala/Algorithm.scala
@@ -1,4 +1,4 @@
-import it.unipd.dei.clustering.{GMM, MapReduceCoreset, Outliers}
+import it.unipd.dei.clustering.{GMM, MapReduceCoreset, Outliers, StreamingCoreset}
 import org.apache.spark.rdd.RDD
 
 object Algorithm {
@@ -12,16 +12,31 @@ object Algorithm {
     GMM.run(coreset.points.map(_.point), k, distance)
   }
 
-  def mapReduce[T](rdd: RDD[T], k: Int, tau: Int, z: Int, distance: (T, T) => Double): IndexedSeq[T] = {
+  def mapReduce[T](rdd: RDD[T], k: Int, tau: Int, z: Int, distance: (T, T) => Double)
+  : (IndexedSeq[T], IndexedSeq[T]) = {
     val coreset = rdd.glom().map { points =>
       MapReduceCoreset.run(points, tau + z, distance)
     }.reduce { case (a, b) =>
       MapReduceCoreset.compose(a, b)
     }
-    val (centers, outliers) = Outliers.run(coreset.points, k, z, distance)
-    // TODO Report number of outliers
-    centers
+    Outliers.run(coreset.points, k, z, distance)
   }
 
+  def streaming[T](stream: Iterator[T], k: Int, tau: Int, distance: (T, T) => Double): IndexedSeq[T] = {
+    val coreset = new StreamingCoreset[T](tau, distance)
+    while(stream.hasNext) {
+      coreset.update(stream.next())
+    }
+    GMM.run(coreset.points.map(_.point), k, distance)
+  }
+
+  def streaming[T](stream: Iterator[T], k: Int, tau: Int, z: Int, distance: (T, T) => Double)
+  : (IndexedSeq[T], IndexedSeq[T]) = {
+    val coreset = new StreamingCoreset[T](tau + z, distance)
+    while(stream.hasNext) {
+      coreset.update(stream.next())
+    }
+    Outliers.run(coreset.points, k, z, distance)
+  }
 
 }
