493d4e4be4441dbe3fd038677cdfc59e66dbb9df
Matteo Ceccarello <ceccarel@dei.unipd.it>
2018-03-09 18:25:18 +0100

    Refine DistanceDistribution

--- experiments/src/main/scala/it/unipd/dei/clustering/DistanceDistribution.scala
+++ experiments/src/main/scala/it/unipd/dei/clustering/DistanceDistribution.scala
@@ -12,6 +12,9 @@ object DistanceDistribution {
     val output = opt[String](required = false, default = Some(input.toOption.get + ".distances"))
     val size = opt[Int](required = false)
     val buckets = opt[Int](default = Some(10))
+    val weightBuckets = opt[Int](default = buckets.toOption)
+    val lowPercentile = opt[Double](default = Some(0.01))
+    val highPercentile = opt[Double](default = Some(0.99))
     verify()
   }
 
@@ -48,30 +51,92 @@ object DistanceDistribution {
 
     println(s"Computing distances between points of the coreset")
     val dCoreset = sc.parallelize(coreset.points)
+      .sortBy(_.weight, ascending = true)
+      .cache()
     val distances = dCoreset.cartesian(dCoreset).map(DistanceWithError.fromTuple).cache()
-
+    println(s"Computed ${distances.count()} distances (sum of weight products = ${distances.map(_.numPairs).sum})")
     val maxDist = distances.max()(Ordering.by(d => d.dist))
 
     println("Computing histogram")
     val (bounds, counts) = distances.map(_.dist).histogram(arguments.buckets())
-    println(s"Bounds (${bounds.length} elems): ${bounds.toSeq}")
-    println(s"Counts (${counts.length} elems): ${counts.toSeq}")
 
     println("Fixing histogram")
+    for (i <- counts.indices) {
+      counts(i) = 0
+    }
     for (derr <- distances.toLocalIterator) {
+      var stop = false
       var i = 0
-      while (i < counts.length && derr.dist <= bounds(i+1)) {
-        if (derr.dist > bounds(i)) {
+      while (i < counts.length && !stop) {
+        if (derr.dist >= bounds(i) && derr.dist < bounds(i+1)) {
           counts(i) += derr.numPairs
+          stop = true
         }
         i += 1
       }
     }
 
     println(s"Maximum distance is $maxDist\n")
-    for (((lb, c), ub) <- bounds.zip(counts).zip(bounds.tail)) {
-      println(s"$lb < d <= $ub :: $c")
+    printHistogram((bounds, counts))
+
+    println("Weight distribution")
+    println("===================")
+
+    printHistogram(dCoreset.map(_.weight).histogram(arguments.weightBuckets()))
+
+    val weights = dCoreset.map(_.weight.toDouble).collect().sorted
+
+    println("Percentiles")
+    for (p <- Seq(0, 0.01, 0.1, 0.25, 0.5, 0.75, 0.9, 0.99, 1.0)) {
+      printPercentile(weights, p)
     }
+
+
+    println("\n\nProxies that cover few points")
+    val lowPercIdx = arguments.lowPercentile() * dCoreset.count()
+
+    val lowPercWeight = dCoreset
+      .zipWithIndex()
+      .filter({case (p, i) => i == lowPercIdx})
+      .collect().head._1.weight
+
+    val potentialOutliers = dCoreset
+      .filter(p => p.weight <= lowPercWeight)
+      .cache()
+
+    println(s"Collected ${potentialOutliers.count()} proxies of potential outliers (weight <= $lowPercWeight)")
+
+    val highPercIdx = arguments.highPercentile() * dCoreset.count()
+
+    val highPercWeight = dCoreset
+      .zipWithIndex()
+      .filter({case (p, i) => i == highPercIdx})
+      .collect().head._1.weight
+
+    val heavyProxies = dCoreset
+      .filter(p => p.weight >= highPercWeight)
+      .cache()
+
+    println(s"Collected ${heavyProxies.count()} heavy proxies (weight >= $highPercWeight)")
+
+    val interestingHist = heavyProxies.cartesian(potentialOutliers).map { case (hp, o) =>
+      distance(hp.point, o.point)
+    }.histogram(arguments.buckets())
+
+    printHistogram(interestingHist)
+
+  }
+
+  def printHistogram(tup: (Array[Double], Array[Long])): Unit = tup match {
+    case (bounds, counts) =>
+      for (((lb, c), ub) <- bounds.zip(counts).zip(bounds.tail)) {
+        println(s"$lb <= x < $ub :: $c")
+      }
+  }
+
+  def printPercentile(arr: Array[Double], perc: Double): Unit = {
+    val idx = math.min(arr.length-1, (perc*arr.length).toInt)
+    println(s"$perc :: ${arr(idx)}")
   }
 
 }
