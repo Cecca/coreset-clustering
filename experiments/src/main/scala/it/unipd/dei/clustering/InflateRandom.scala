9c2ecf909d236fc22799321c62f23691bfcace2e
Matteo Ceccarello <ceccarel@dei.unipd.it>
2018-04-01 11:20:17 +0200

    Add a program to inflate randomly

---
+++ experiments/src/main/scala/it/unipd/dei/clustering/InflateRandom.scala
@@ -0,0 +1,55 @@
+package it.unipd.dei.clustering
+
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.{SparkConf, SparkContext}
+import org.rogach.scallop.ScallopConf
+
+import scala.util.Random
+
+object InflateRandom {
+
+  class Opts(args: Array[String]) extends ScallopConf(args) {
+
+    val input = opt[String](required=true)
+    val output = opt[String](required=true)
+    val sizeFactor = opt[Int](required=true)
+    val deviationFactor = opt[Double](required=false, default=Some(0.01))
+
+    verify()
+  }
+
+  def main(args: Array[String]): Unit = {
+    val opts = new Opts(args)
+
+    val sparkConf = new SparkConf(loadDefaults = true).setAppName("Clustering")
+    val sc = new SparkContext(sparkConf)
+
+    val original = VectorIO.readKryo(sc, opts.input())
+
+    val mins = original.map(_.toArray).reduce({case (a1, a2) =>
+      a1.zip(a2).map({case (x, y) => math.min(x, y)})
+    })
+    val maxs = original.map(_.toArray).reduce({case (a1, a2) =>
+      a1.zip(a2).map({case (x, y) => math.max(x, y)})
+    })
+
+    val deviations = mins.zip(maxs).map({ case (x, y) =>
+      opts.deviationFactor() * math.abs(x-y)
+    })
+
+    val sizeFactor = opts.sizeFactor()
+
+    val inflated = original.glom().flatMap({ vecs =>
+      (0 until (vecs.length * (sizeFactor-1))).map({ _ =>
+        val data = vecs(Random.nextInt(vecs.length)).copy.toArray
+        for (i <- deviations.indices) {
+          data(i) += Random.nextGaussian() * deviations(i)
+        }
+        Vectors.dense(data)
+      }) ++ vecs
+    })
+
+    VectorIO.writeKryo(inflated, opts.output())
+  }
+
+}
